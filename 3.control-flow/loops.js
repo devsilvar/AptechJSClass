6d94351161e2a71ccb7b262452d2eb0d92b2d84f
Yusuf <refsilva@yahoo.com>
2024-03-11 18:33:31 +0100

    added looop file in control flow

---
+++ 3.control-flow/loops.js
@@ -0,0 +1,204 @@
+// LOOPS
+// console.log( 1 )
+// console.log( 2 )
+// console.log( 3 )
+// console.log( 4 )
+// console.log( 5 )
+// console.log( 6 )
+// console.log( 7 )
+// console.log( 8 )
+// console.log( 9 )
+// console.log( 10 )
+
+
+// DO while
+// while
+// For
+
+// let num = 5;
+// num = 6;
+// num = 7;
+
+// num = num + 2;
+
+// console.log( num )
+
+
+
+
+let startingNumber = 0;
+do {
+  // how we are going to inccreae the starting Number
+  startingNumber = startingNumber + 1;
+  // console.log( startingNumber )
+} while ( startingNumber <= 10 )
+
+
+
+let startingNum = 10;
+
+do {
+  // how are we icnresing the number
+  startingNum = startingNum + 2;
+  //console.log( startingNum )
+} while ( startingNum <= 20 )
+
+
+let startOutput = 1
+do {
+  startOutput = startOutput + 1;
+  //console.log( "harrison" )
+} while ( startOutput <= 10 )
+
+
+
+let evenNum = 0;
+
+while ( evenNum <= 20 ) {
+  //console.log( evenNum )
+  //how do we want to increse it
+  evenNum = evenNum + 2
+}
+
+// for(startingPOint; EndingPoint; How we want to increase it){
+
+// }
+
+for ( let i = 0; i < 10; i = i + 1 ) {
+  //console.log( i )
+}
+
+
+// Write a Program that Perfroms the addition of Number from 1 - 5
+
+let total = 0;
+for ( let i = 0; i <= 5; i = i + 1 ) {
+  total = total + i
+}
+//console.log( total )
+
+
+
+
+// num = num + 1
+
+
+// num += 2;
+// num = num + 2;
+
+
+// num = num - 2;
+// num -= 2;
+
+let num = 1;
+
+
+num++
+num++
+//console.log( num )
+
+// // Continue && Break
+
+
+for ( let i = 0; i < 10; i++ ) {
+  //console.log( i )
+  if ( i == 7 ) {
+    break;
+  }
+}
+
+
+
+for ( let i = 0; i < 10; i++ ) {
+  if ( i == 5 ) {
+    continue;
+  } else {
+    // console.log( i )
+  }
+}
+
+
+
+
+
+
+let odd = 1;
+
+do {
+  console.log( odd )
+  odd = odd + 2
+} while ( odd < 10 )
+
+
+
+for ( let i = 1; i < 10; i = i + 2 ) {
+  console.log( i )
+}
+
+
+/**
+ * For loop
+ * While loop
+ * Do..while loop
+ */
+
+// FOR LOOP
+// Breakdown of the For loop syntax
+/**
+ * The initialExpression initializes and/or declares variables and executes only once.
+ * The condition is evaluated.
+ *  - If the condition is false, the for loop is terminated.
+ *  - If the condition is true, the block of code inside of the for loop is executed.
+ * The expressionUpdate updates the value of initialExpression when the condition is true.
+ * The condition is evaluated again. This process continues until the condition is false.
+*/
+
+// for (initialExpression; condition; expressionUpdate (increment / decrement)) {
+//     // for loop body
+// }
+
+// Log out numbers 1 - 100
+// console.log( 1 );
+// console.log( 2 );
+// console.log( 3 );
+// console.log( 1 );
+// console.log( 4 );
+// console.log( 5 );
+// console.log( 6 );
+// console.log( 7 );
+// console.log( 8 );
+// console.log( 9 );
+// console.log( 10 );
+
+// // Log out numbers 1 - 100
+// for ( let index = 1; index <= 100; index++ ) {
+//   console.log( index );
+// }
+
+// // Using for loop to Log out 'I love JavaScript' 7 times
+// for ( let index = 1; index < 8; index++ ) {
+//   console.log( "I love JavaScript" );
+// }
+
+// // Log out the sum of numbers from 1 to 5
+// console.log( 1 + 2 + 3 + 4 + 5 );
+
+// // Using for loop to log out sum of numbers
+// let sum = 0;
+
+// for ( let index = 1; index <= 500; index++ ) {
+//   sum += index; // sum = sum + index;
+// }
+// console.log( "Sum is " + sum );
+
+
+
+
+
+
+
+
+
+
+
+
