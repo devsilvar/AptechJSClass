b8126a5f00c6c881ac6dfd7e3e9c0076b8c6ee40
Yusuf <refsilva@yahoo.com>
2024-04-15 12:13:51 +0100

    aihsciusa

--- regular expression/regex.js
+++ regular expression/regex.js
@@ -1,12 +1,13 @@
+
 let format = "###-###-###"
 let exmapleNumber = "895-507-897"
-
 
 
 function checkPattern ( exampleNum ) {
   //convert it to an array
   exampleNum = exampleNum.split( '' )
 
+  
   //map through the array
   let newArr = exampleNum.map( ( item, index, arr ) => {
     if ( item >= 0 ) {
@@ -47,40 +48,29 @@ console.log( answer1 )
 // we can use the first method or the second method
 
 let pattern = /boy/.test( "the name of the boy is destiny" )
-
 let pattern2 = new RegExp( "boy", "g" )
 
+
 console.log( pattern )
-
-
-
-
-
-
-
-
-
 
 function checkpattern2 ( checkNum ) {
   let pattern = /^\d{3}-\d{3}-\d{3}$/;
   return pattern.test( checkNum )
 }
 
+
+
 let answer2 = checkpattern2( exmapleNumber )
-
 console.log( answer2 )
-
 // when you have $ at the end of a ReportingObserver, it matches the string from the end
-
 // e.g '/art$/ wil match cart not artyu'
-
 // putting /b at the end of a regex means it will match any varitaion of that astring
 
 
 // REGEX EXAMPLES
-
 // check if the word a,b, c is in a sentence 
 [ abc ]  => /abc/
+
 
 // check for non vowels word
 [^ aeiou ]
@@ -111,39 +101,34 @@ console.log( answer2 )
 // targets spaces
 
 // WAYS OF CREATING A REGEX EXPRESION
-
 // without flags
 
-let regexpresion = /abcde/;
-
-// let regexpression2 = new RegExp("patter", [, flags]);
+let regexpresion = /abcde/flag;
+let regexpression2 = new RegExp("patter", [, flags]);
 
 // Diffences and when to USE
 
 // Use the one without flag whne you know the regular expression you are testing against
-
 // use the one with flag whne you don;t know the input you are testing against whie writing the code
 
 // WHAT ARE FLAGS
 
 // -- g is used for global search which means the search will not return after the first match.
-
 // -- i is used for case-insensitive search meaning that a match can occur regardless of the casing.
-
 // -- m is used for multiline search.
-
 // -- u is used for Unicode search.
 
 let wordToTest = "you are the one you are waiting for";
 
 //lets look for the word "you"
-
 let regularExpressionLiteral = /you/gi;
 // means checl for the word you continiously
 let answer = wordToTest.match( regularExpressionLiteral );
 
 console.log( answer );
 
+
+// classwork
 // if you were to use loop here is how you might have to do it
 function checkforYOU ( userInput ) {
   let getValue = [];
@@ -173,27 +158,22 @@ let ans1 = wordToTest.match( regEX );
 console.log( ans1 );
 
 // SPECIAL CHARACTERS
-
 // ^^^^^^
 
 const regexPattern1 = /^cat/;
 
 console.log( regexPattern1.test( "cat and mouse" ) ); // Output: true
-
 console.log( regexPattern1.test( "The cat and mouse" ) ); // Output: false because the line does not start with cat
 
 // Without the ^ in the pattern, the output will return true
 // because we did not assert a boundary.
 
 const regexPattern2 = /cat/;
-
 console.log( regexPattern2.test( "The cat and mouse" ) ); // Output: true
 
 // &&&&&&&
 const regexPattern = /cat$/;
-
 console.log( regexPattern.test( "The mouse and the cat" ) ); // Output: true
-
 console.log( regexPattern.test( "The cat and mouse" ) ); // Output: false
 
 // when we use the flag \b we match every word that end in the specified word
@@ -217,7 +197,6 @@ let regEx3 = /war\b/g  "match every word that starts with war in a sentence"
 
 
 // CHARACTER CLASS
-
 // when we say character class, maybe we want to match number, small letters , capiatal letters, special characters,
 
 // There are clasese of character, now, how do we match them
@@ -240,5 +219,9 @@ for number[ 0 - 9 ]
 
 
 
-// REGEX FOR EMAIL
-const regexPatternForEmail = /^[(\w\d\W)+]+@[\w+]+\.[\w+]+$\i/
+
+
+// // REGEX FOR EMAIL
+// const regexPatternForEmail = /^[(\w\d\W)+]+@[\w+]+\.[\w+]+$\i/
+
+
